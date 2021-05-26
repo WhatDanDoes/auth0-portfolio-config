a976283469f038a7e18d32f3ea5e27db737f8f47
Daniel Bidulock <daniel_bidulock@sil.org>
2021-05-26 16:34:38 -0600

    Remove configured client_metadata requirement

--- settings/rules/Add email verified to access token.js
+++ settings/rules/Add email verified to access token.js
@@ -1,11 +1,12 @@
 function (user, context, callback) {
-    // This rule adds the authenticated user's email address to the access token.
-    if (context.clientMetadata.isAudioManagerApp !== 'true') {
-      return callback(null, user, context);
-    }
-  
-    var namespace = 'https://sil.org/';
-  
-    context.accessToken[namespace + 'email_verified'] = user.email_verified;
+  // This rule adds the authenticated user's email address to the access token.
+  if (context.clientName !== 'Audio Manager Desktop') &&
+      context.clientName !== 'Audio Manager') {
     return callback(null, user, context);
   }
+
+  var namespace = 'https://sil.org/';
+
+  context.accessToken[namespace + 'email_verified'] = user.email_verified;
+  return callback(null, user, context);
+}
