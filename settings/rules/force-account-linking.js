cdf5ce5e4e03bea74fd4d84d1148a2a7e2e40449
Daniel Bidulock <daniel_bidulock@sil.org>
2021-08-05 17:07:41 -0600

    Add more details to docs

--- settings/rules/force-account-linking.js
+++ settings/rules/force-account-linking.js
@@ -1,20 +1,21 @@
 /**
  * This rule forces accounts registered under identical emails to be _linked_
  * at Auth0. Though Auth0 provides an extension to give an authenticated agent
- * the option to link accounts, this rule supersedes that functionality. Nowhere
- * is this recommended by Auth0:
+ * the option to link accounts, this rule supersedes that functionality.
  *
- * https://auth0.com/docs/users/user-account-linking
+ * This may best represent what Auth0 considers best practices:
  *
- * This rule also contravenes Auth0-recommended best practices for searching
+ *    https://auth0.com/docs/users/user-account-linking
+ *
+ * This rule may also contravene Auth0-recommended best practices for searching
  * profiles:
  *
- * https://auth0.com/docs/rules/use-management-api#access-a-newer-version-of-the-library
+ *    https://auth0.com/docs/rules/use-management-api#access-a-newer-version-of-the-library
  *
  * From the link above:
  *
- * _Searching for users from inside Rules may affect the performance of your
- * logins; we advise against it_
+ *    > Searching for users from inside Rules may affect the performance of your
+ *    logins; we advise against it
  */
 function (user, context, callback) {
 
