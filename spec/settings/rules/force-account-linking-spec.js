2dacf90b2d1c5577e3a0565e90ed7d57b8ea7469
Daniel Bidulock <daniel_bidulock@sil.org>
2021-08-05 13:12:49 -0600

    Fill test expectations

--- spec/settings/rules/force-account-linking-spec.js
+++ spec/settings/rules/force-account-linking-spec.js
@@ -15,9 +15,14 @@ describe('force-account-linking', () => {
 
   let rule, agent, context;
 
+  let getUsersByEmailSpy, linkUsersSpy;
+
   beforeEach(done => {
     agent = { ..._agent };
     context = { ..._context, accessToken: {} };
+
+    getUsersByEmailSpy = jasmine.createSpy('getUsersByEmailSpy');
+    linkUsersSpy = jasmine.createSpy('linkUsersSpy');
 
     fs.readFile('./settings/rules/force-account-linking.js', 'utf8', (err, data) => {
       if (err) return done.fail(err);
@@ -25,7 +30,12 @@ describe('force-account-linking', () => {
       function makeFunc() {
 
         let configuration = {};
-        let auth0 = {};
+        let auth0 = {
+          users: {
+            getUsersByEmail: getUsersByEmailSpy,
+            linkUsers: linkUsersSpy,
+          },
+        };
 
         let func;
         eval('func = ' + data);
@@ -39,78 +49,221 @@ describe('force-account-linking', () => {
   });
 
   describe('no linkable accounts', () => {
+    beforeEach(() => {
+      getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+        cb(null, [{...agent}]);
+      });
+    });
 
     it('calls find users-by-email endpoint', done => {
-      done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+        expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+        done();
+      });
     });
 
     it('does not call the link accounts endpoint', done => {
-      done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(linkUsersSpy).not.toHaveBeenCalled();
+        expect(linkUsersSpy.calls.count()).toEqual(0);
+        done();
+      });
     });
 
     describe('accounts are already linked', () => {
+      beforeEach(() => {
+        getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+          cb(null, [{...agent, identities: [agent.identities[0], {
+            "profileData": {
+              "email": agent.email,
+              "email_verified": true,
+              "name": "Some Guy",
+              "given_name": "Some",
+              "family_name": "Guy",
+              "picture": "https://lh3.googleusercontent.com/a-/.jpg",
+              "gender": "male",
+              "locale": "en"
+            },
+            "user_id": "113710000000000000000",
+            "provider": "google-oauth2",
+            "connection": "google-oauth2",
+            "isSocial": true
+            }]
+          }]);
+        });
+      });
 
       it('calls find users-by-email endpoint', done => {
-        done.fail();
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+          expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+          done();
+        });
       });
 
       it('does not call the link accounts endpoint', done => {
-        done.fail();
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(linkUsersSpy).not.toHaveBeenCalled();
+          expect(linkUsersSpy.calls.count()).toEqual(0);
+          done();
+        });
       });
     });
 
     describe('account was manually unlinked', () => {
+      beforeEach(() => {
+        getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+          cb(null, [{...agent, user_metadata: { manually_unlinked: true } }]);
+        });
+      });
 
       it('calls find users-by-email endpoint', done => {
-        done.fail();
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+          expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+          done();
+        });
       });
 
       it('does not call the link accounts endpoint', done => {
-        done.fail();
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(linkUsersSpy).not.toHaveBeenCalled();
+          expect(linkUsersSpy.calls.count()).toEqual(0);
+          done();
+        });
       });
     });
   });
 
   describe('one linkable account', () => {
 
+    let identity;
+    beforeEach(() => {
+      identity = {
+        "user_id": "113710000000000000000",
+        "provider": "google-oauth2",
+        "connection": "google-oauth2",
+        "isSocial": true
+      };
+
+      getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+        cb(null, [{...agent}, {...agent, name: 'Some Guy', identities: [identity] }]);
+      });
+    });
+
     it('calls find users-by-email endpoint', done => {
-      done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+        expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+        done();
+      });
     });
 
     it('calls the link accounts endpoint', done => {
-      done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(linkUsersSpy).toHaveBeenCalledWith(agent.user_id, {user_id: identity.user_id, provider: identity.provider}, jasmine.any(Function));
+        expect(linkUsersSpy.calls.count()).toEqual(1);
+        done();
+      });
     });
   });
 
   describe('several linkable accounts', () => {
 
+    let identity1, identity2;
+    beforeEach(() => {
+      identity1 = {
+        "user_id": "113710000000000000000",
+        "provider": "google-oauth2",
+        "connection": "google-oauth2",
+        "isSocial": true
+      };
+      identity2 = {
+        "user_id": "paratext-audiomanager|WfCxeyQQi00000000",
+        "provider": "oauth2",
+        "connection": "paratext-audiomanager",
+        "isSocial": true
+      };
+
+      getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+        cb(null, [
+          {...agent},
+          {...agent, name: 'Some Guy', identities: [identity1] },
+          {...agent, name: 'Same Goy', identities: [identity2] }
+        ]);
+      });
+    });
+
     it('calls find users-by-email endpoint', done => {
-      done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+        expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+        done();
+      });
     });
 
     it('calls the link accounts endpoint for each of the linkable accounts', done => {
-      done.fail();
-    });
-
-    describe('with account already linked', () => {
-
-      it('calls find users-by-email endpoint', done => {
-        done.fail();
-      });
-
-      it('does not call the link-accounts endpoint for the account already linked', done => {
-        done.fail();
+      rule(agent, context, (err, agnt, cntxt) => {
+        if (err) return done.fail(err);
+        expect(linkUsersSpy.calls.count()).toEqual(2);
+        expect(linkUsersSpy).toHaveBeenCalledWith(agent.user_id, {user_id: identity1.user_id, provider: identity1.provider}, jasmine.any(Function));
+        expect(linkUsersSpy).toHaveBeenCalledWith(agent.user_id, {user_id: identity2.user_id, provider: identity2.provider}, jasmine.any(Function));
+        done();
       });
     });
 
     describe('with manually_unlinked account', () => {
 
-      it('calls find users-by-email endpoint', done => {
-        done.fail();
+      let identity1, identity2;
+      beforeEach(() => {
+        identity1 = {
+          "user_id": "113710000000000000000",
+          "provider": "google-oauth2",
+          "connection": "google-oauth2",
+          "isSocial": true
+        };
+        identity2 = {
+          "user_id": "paratext-audiomanager|WfCxeyQQi00000000",
+          "provider": "oauth2",
+          "connection": "paratext-audiomanager",
+          "isSocial": true
+        };
+  
+        getUsersByEmailSpy = getUsersByEmailSpy.and.callFake(function(email, cb) {
+          cb(null, [
+            {...agent},
+            {...agent, name: 'Some Guy', identities: [identity1], user_metadata: { manually_unlinked: true } },
+            {...agent, name: 'Same Goy', identities: [identity2] }
+          ]);
+        });
       });
 
-      it('does not call the link-accounts endpoint for the manually_linked account', done => {
-        done.fail();
+      it('calls find users-by-email endpoint', done => {
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(getUsersByEmailSpy).toHaveBeenCalledWith(agent.email, jasmine.any(Function));
+          expect(getUsersByEmailSpy.calls.count()).toEqual(1);
+          done();
+        });
+      });
+
+      it('does not call the link-accounts endpoint for the manually_unlinked account', done => {
+        rule(agent, context, (err, agnt, cntxt) => {
+          if (err) return done.fail(err);
+          expect(linkUsersSpy.calls.count()).toEqual(1);
+          expect(linkUsersSpy).toHaveBeenCalledWith(agent.user_id, {user_id: identity2.user_id, provider: identity2.provider}, jasmine.any(Function));
+          done();
+        });
       });
     });
   });
